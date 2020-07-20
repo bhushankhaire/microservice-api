0a6f4782f111a988158d12f00f7343aa7a22f483
L138113 <bhushan.khaire@westpac.com.au>
2020-07-20 11:41:24 +0530

    commit code

---
+++ src/main/java/spring/demo/controller/HelloController.java
@@ -0,0 +1,13 @@
+package spring.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping(path = "/hello")
+	public String hellWorld() {
+		return "hello World";
+	}
+}
